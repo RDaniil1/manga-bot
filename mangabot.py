0168eef0aa71edda8c8c2c2357e179b0f9f37678
RDaniil1 <43912308+RDaniil1@users.noreply.github.com>
2025-02-27 10:51:41 +0000

    main: update mangabot.py

--- mangabot.py
+++ mangabot.py
@@ -5,15 +5,11 @@ bot = telebot.TeleBot('')
 
 @bot.message_handler(commands=["start", "help"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Пришлите ссылку на главную страницу манги с readmanga.io. Дополнительно можно указать том и главу манги')
+    bot.send_message(m.chat.id, 'Отправьте ссылку на главную страницу манги с readmanga.io. Дополнительно можно указать том и главу манги')
 
 @bot.message_handler(regexp=r'https://readmanga.io/')
 def get_manga(message):
-    # try:
     data = message.text.split()
-    # except Exception:
-    #     bot.send_message(message.chat.id, 'Недостаточно данных')
-    #     return  
 
     manga_list = get_manga_from_vol_num(len(data), *data)
 
@@ -23,6 +19,5 @@ def get_manga(message):
 
     for manga in manga_list:
         bot.send_document(message.chat.id, document=open(manga, 'rb'))
-
 
 bot.polling(none_stop=True, interval=0)
